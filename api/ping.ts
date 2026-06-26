0385500244774b63720843a7c02d875cf023e8a5
Indrajit Kumar <kumarindrajitcricket@gmail.com>
2026-06-26 23:02:50 +0530

    ping: add eq/zod/crypto static imports to isolate crash

--- api/ping.ts
+++ api/ping.ts
@@ -1,5 +1,8 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { db } from "./_db";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+import crypto from "crypto";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
